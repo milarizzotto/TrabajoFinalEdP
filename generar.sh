9caa1035c41d5bd1ddd80d4084ef5efdfac3f5eb
CasadoPedro <casado.pedro.martin@gmail.com>
2023-06-20 23:12:31 -0300

    Añade funcionalidad descargar y renombrar imágenes

--- generar.sh
+++ generar.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 function generar(){
-	echo "$1"
+	if [ ! -e ./nombres/dict.csv ]
+        then
+        	wget -P ./nombres https://raw.githubusercontent.com/fernandezpablo85/name_suggestion
+        fi
 	for (( i=0; i<$1; i++ ))
 	do
-	wget -P ./imagenes https://source.unsplash.com/random/900%C3%97700/?person
-	sleep 2
+	numRandom=$(shuf -i 0-96000 -n 1)
+	p='p'
+	num="${numRandom}${p}"
+	nombre=$(sed -n $num ./nombres/dict.csv | grep -o "^[^,]*" | sed 's/ /_/g')
+	wget -P ./imagenes/ https://source.unsplash.com/random/900%C3%97700/?person
+        mv ./imagenes/index.html?person ./imagenes/$nombre.jpeg
+        sleep 1
 	done
 }
