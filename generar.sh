92ac2a7cf8ec0df27648eba70cc2caf929420b71
Camila Rizzotto <mariacamilarizzotto@hotmail.com>
2023-06-28 22:08:06 -0300

    Termina funcion generar

--- generar.sh
+++ generar.sh
@@ -3,7 +3,7 @@
 function generar(){
 	if [ ! -e ./nombres/dict.csv ]
         then
-        	wget -P ./nombres https://raw.githubusercontent.com/fernandezpablo85/name_suggestion
+        	wget -P ./nombres  https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv
         fi
 	for (( i=0; i<$1; i++ ))
 	do
@@ -14,5 +14,10 @@ function generar(){
 	wget -P ./imagenes/ https://source.unsplash.com/random/900%C3%97700/?person
         mv ./imagenes/index.html?person ./imagenes/$nombre.jpeg
         sleep 1
-	done
+	don
+	cd ./imagenes
+	zip imgcomprimidas *
+	rm *.jpeg
+	md5sum imgcomprimidas.zip > suma_verificacion.txt
+	cd ..
 }
